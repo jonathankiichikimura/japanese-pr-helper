fb658d0b345233936c410148fc82a657db3d8d8d
Steven Sawadisavi <stevesavi+lewagon@gmail.com>
2026-02-25 07:06:02 +0900

    added code for adding chats on create

--- app/controllers/user_applications_controller.rb
+++ app/controllers/user_applications_controller.rb
@@ -11,6 +11,7 @@ class UserApplicationsController < ApplicationController
     @user_application = UserApplication.new(user_application_params)
     @user_application.user = current_user
     if @user_application.save
+      populate_chats(@user_application)
       redirect_to new_user_application_chat_path(@user_application)
     else
       render :new, status: :unprocessable_entity
@@ -18,6 +19,34 @@ class UserApplicationsController < ApplicationController
   end
 
   private
+
+  def populate_chats(user_application)
+    case user_application.application_journey.application_road
+    when "married"
+      titles = [
+        "Application Form",
+        "Statement of Reasons",
+        "Letter of Guarantee",
+        "Employment Certificate",
+        "Taxation Certificate",
+        "Tax payment Certificate",
+        "Monthly Pension Record"
+      ]
+    when "long_term"
+      titles = []
+    when "work"
+      titles = []
+    when "highly"
+      titles = []
+    when "special"
+      titles = []
+    end
+    titles.each do |title|
+      chat = Chat.new(title: title)
+      chat.user_application = user_application
+      chat.save
+    end
+  end
 
   def user_application_params
     params.require(:user_application).permit(:application_journey_id, :title)
