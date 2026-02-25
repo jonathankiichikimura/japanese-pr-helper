15ea39b9159100a48680b745171b555144c7971a
Raphael Barbosa <sp1aca9fa@gmail.com>
2026-02-25 16:02:14 +0900

    changed message assignments back to create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -13,6 +13,8 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_params)
+    @message.chat = @chat
+    @message.role = "user"
 
     if @message.save
       llm_response
@@ -89,7 +91,5 @@ class MessagesController < ApplicationController
   # Unrelated to above / setting message_params to avoid unwanted columns
   def message_params
     params.require(:message).permit(:content)
-    @message.chat = @chat
-    @message.role = "user"
   end
 end
